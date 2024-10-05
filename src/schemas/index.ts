39a5f2de98f4b104e9a48abef02e34cfdc4fcfd7
amitgt9967 <amitgt9967@gmail.com>
2024-10-05 21:51:54 +0530

    added user schema and prisma config in nextjs

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,12 @@
 import * as z from "zod";
 
-
 export const LoginSchema = z.object({
-    email: z.string({ invalid_type_error: "Must be a string" }).email({ message: "Email is required" }),
-    password: z.string().min(1, { message: "Password is required"}).max(255),
+  email: z.string({ invalid_type_error: "Must be a string" }).email({ message: "Email is required" }),
+  password: z.string().min(1, { message: "Password is required" }).max(255),
 });
 
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  password: z.string().min(6, { message: "Minimum is required" }),
+  name: z.string().min(1, { message: "Name is required" })
+});
